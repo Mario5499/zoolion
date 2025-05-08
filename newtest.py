d53115388a129e52e6dc516fc6b747a3aef34454
Mario <bashgular@protonmail.com>
2025-05-08 18:29:44 +0530

    Update newtest.py

--- newtest.py
+++ newtest.py
@@ -1,27 +1,24 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-from selenium.common.exceptions import WebDriverException, TimeoutException
-import subprocess
 import time
-import os
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
-print("Script started")
 
-options =  webdriver.ChromeOptions()
+
+options = webdriver.ChromeOptions()
 options.binary_location = "/usr/bin/chromium-browser"
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-
-
-
+options.add_argument("--window-size=1345x610")
 driver = webdriver.Chrome(options=options)
+driver.set_window_size(1354, 610)
+actions = ActionChains(driver)
+print("script started")
 
-
-driver.get("www.google.com")
-time.sleep(5)
-
+driver.get("https://rrkb.in.net/code/Pytho_code_8.html")
+print("Done")
 print("done")
+
+
+driver.quit()
